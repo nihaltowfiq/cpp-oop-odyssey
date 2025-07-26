0b17b682e35a2c7f51fd592bae9b25406e85939e
nihaltowfiq <nihaltowfiq@gmail.com>
2025-07-27 00:33:41 +0600

    01. added

---
+++ 01.basics/01-class_vs_struct.cpp
@@ -0,0 +1,42 @@
+/*
+  ## Classes vs Structs in C++
+
+  - Both can have data members and member functions.
+  - **Class**: Default access specifier is **private**.
+  - **Struct**: Default access specifier is **public**.
+  - Use class when implementing OOP concepts.
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class Car {
+public:
+  string brand;
+  int year;
+
+  void display() { cout << "Brand: " << brand << ", Year: " << year << endl; }
+};
+
+struct Bike {
+  string brand;
+  int year;
+
+  void display() { cout << "Brand: " << brand << ", Year: " << year << endl; }
+};
+
+int main() {
+  Car car1;
+  car1.brand = "Audi";
+  car1.year = 2020;
+  car1.display();
+
+  Bike bike1;
+  bike1.brand = "Yamaha";
+  bike1.year = 2022;
+  bike1.display();
+
+  return 0;
+}
